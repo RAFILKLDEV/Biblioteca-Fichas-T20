a276fc1ccf7a90622c7b7c8fc1691d322b91da9f
Caio Cabral <ocaiocabral@gmail.com>
2022-12-02 17:11:57 -0400

    Impedindo scroll do body quando modal estiver acionado

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,9 +1,13 @@
 import './main.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import ModalPesquisa from '../ModalPesquisa';
 
 const Main = () => {
    const [mostrar, setMostrar] = useState(false);
+
+   useEffect(() => {
+      mostrar === true ? document.body.style.overflow='hidden' : document.body.style.overflow='unset'
+   }, [mostrar])
 
    function mostraModal(){
       setMostrar(true);
--- src/components/ModalPesquisa/index.js
+++ src/components/ModalPesquisa/index.js
@@ -4,8 +4,12 @@ const ModalPesquisa = ({mostrar, esconderModal}) => {
    
    
    return(
-      <div className={`modal-pesquisa ${mostrar ? 'mostrar' : ''}`}>
+      <div className={`modal__fundo ${mostrar ? 'mostrar' : ''}`} onClick={esconderModal}>
+      <div className={`modal__pesquisa ${mostrar ? 'mostrar' : ''}`}>
+         <input type={'radio'} />
+         <button>Pesquisar</button>
          <button onClick={esconderModal}>Cancelar</button>
+      </div>
       </div>
    )
 }
